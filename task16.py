4c5cb686bcd752e93376944301a783175ca44385
Ashok Nadiminti <ashoknadiminti6305@gmail.com>
2026-06-23 19:10:09 +0530

    7th commit

---
+++ task16.py
@@ -0,0 +1,71 @@
+# 1. int -> float
+float(10)
+
+# 2. int -> str
+str(10)
+
+# 3. int -> bool
+bool(10)
+
+# 4. float -> int
+int(10.5)
+
+# 5. float -> str
+str(10.5)
+
+# 6. float -> bool
+bool(10.5)
+
+# 7. str -> int
+int("10")
+
+# 8. str -> float
+float("10.5")
+
+# 9. str -> bool
+bool("Hello")
+
+# 10. bool -> int
+int(True)
+
+# 11. bool -> float
+float(True)
+
+# 12. bool -> str
+str(True)
+
+# 13. list -> tuple
+tuple([1,2,3])
+
+# 14. list -> set
+set([1,2,3])
+
+# 15. list -> str
+str([1,2,3])
+
+# 16. tuple -> list
+list((1,2,3))
+
+# 17. tuple -> set
+set((1,2,3))
+
+# 18. tuple -> str
+str((1,2,3))
+
+# 19. set -> list
+list({1,2,3})
+
+# 20. set -> tuple
+tuple({1,2,3})
+
+# 21. set -> str
+str({1,2,3})
+
+# 22. str -> list
+list("Python")
+
+# 23. str -> tuple
+tuple("Python")
+
+# 24. str -> set
+set("Python")
