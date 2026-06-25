cc1977e411bf76c86e957a63a6b2e9b61e33f21c
Ashok Nadiminti <ashoknadiminti6305@gmail.com>
2026-06-25 22:05:17 +0530

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        
+        while(num>=10):
+            x=0
+            while(num>0):
+
+                d=num%10
+                x+=d
+                num//=10
+            num=x
+        return num
